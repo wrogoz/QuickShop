075527ac5eb21dd445cacadc4069c0f6e7943036
Wojciech Rogoz <wrogoz@gmail.com>
2020-10-22 12:16:04 +0200

    heroku cors isue

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app=express()
 const db = require('./db/db')
 const helmet = require("helmet")
-
+const setHeaders = require('./middleware/setHeaders')
 const user = require('./routes/userRoute')
 const cors = require("cors")
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
+app.use(setHeaders)
 app.use('/user',user)
 
 const port = process.env.PORT || 8000;
