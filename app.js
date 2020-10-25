95ee0db0a6b9d3d291e5aa14f7360c0969a73828
Wojciech Rogoz <wrogoz@gmail.com>
2020-10-25 09:26:29 +0100

    cors issue in progrss

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express')
-const app=express()
 require('dotenv').config()
+const app=express()
 const db = require('./db/db')
 const helmet = require("helmet")
+
 const setHeaders = require('./middleware/setHeaders')
 const user = require('./routes/userRoute')
 const cors = require("cors")
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
-
+app.use(setHeaders)
 app.get('/',(req,res)=>{
     res.send('quickshop app documentation test')
   })
-  
 app.use('/user',user)
 
 const port = process.env.PORT || 8000;
