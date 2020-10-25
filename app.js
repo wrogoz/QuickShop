fadac59530c0419b8d1b6c340f53dddd414086ec
Wojciech Rogoz <wrogoz@gmail.com>
2020-10-25 09:46:06 +0100

    heroku issue still in progress

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const helmet = require("helmet")
 const setHeaders = require('./middleware/setHeaders')
 const user = require('./routes/userRoute')
 const cors = require("cors")
-app.use(helmet())
 app.use(cors())
+app.use(helmet())
 app.use(express.json())
-app.use(setHeaders)
+
 app.get('/',(req,res)=>{
     res.send('quickshop app documentation test')
   })
