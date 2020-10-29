14803e178bcf2fc4171127d81e14bc919a0c0078
Wojciech Rogoz <wrogoz@gmail.com>
2020-10-29 18:17:50 +0100

    x

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ const auth = async(req,res,next)=>{
         if(!token){
             res.status(400).send({error:'no token provided'})
         }else{
-            const decodedToken=jwt.verify(token,proces.env.JWTSECRET);
+            const decodedToken=jwt.verify(token,process.env.JWTSECRET);
             req.body.user=decodedToken;
             
             next();
