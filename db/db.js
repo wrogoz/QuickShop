8236a4603c83726b6a8b1f2013c607399a678064
Wojciech Rogoz <wrogoz@gmail.com>
2020-10-29 18:37:44 +0100

    hiding d

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect(`mongodb+srv://wrogoz:${process.env.DBPASS}@cluster0.rmad5.mongodb.net/quickshop?retryWrites=true&w=majority`, {useNewUrlParser: true,useUnifiedTopology:true,useCreateIndex:true})
+mongoose.connect(`${process.env.URI}`, {useNewUrlParser: true,useUnifiedTopology:true,useCreateIndex:true})
 .then(()=>{
     console.log('db connected')
     
